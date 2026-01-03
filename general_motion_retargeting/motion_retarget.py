943fb35fa655379c6b331f34d848d56e98cf5746
Tom Stewart <tom.stewart@woven.toyota>
2026-01-03 10:12:37 +0900

    Tidy up motion_retarget a little bit

--- general_motion_retargeting/motion_retarget.py
+++ general_motion_retargeting/motion_retarget.py
@@ -25,38 +25,38 @@ class GeneralMotionRetargeting:
         self.xml_file = str(ROBOT_XML_DICT[tgt_robot])
         if verbose:
             print("Use robot model: ", self.xml_file)
+
         self.model = mj.MjModel.from_xml_path(self.xml_file)
 
-        # Print DoF names in order
-        print("[GMR] Robot Degrees of Freedom (DoF) names and their order:")
-        self.robot_dof_names = {}
-        for i in range(self.model.nv):  # 'nv' is the number of DoFs
-            dof_name = mj.mj_id2name(
-                self.model, mj.mjtObj.mjOBJ_JOINT, self.model.dof_jntid[i]
-            )
-            self.robot_dof_names[dof_name] = i
-            if verbose:
-                print(f"DoF {i}: {dof_name}")
+        self.robot_dof_names = {
+            mj.mj_id2name(self.model, mj.mjtObj.mjOBJ_JOINT, self.model.dof_jntid[i]): i
+            for i in range(self.model.nv)
+        }
 
-        print("[GMR] Robot Body names and their IDs:")
-        self.robot_body_names = {}
-        for i in range(self.model.nbody):  # 'nbody' is the number of bodies
-            body_name = mj.mj_id2name(self.model, mj.mjtObj.mjOBJ_BODY, i)
-            self.robot_body_names[body_name] = i
-            if verbose:
-                print(f"Body ID {i}: {body_name}")
+        self.robot_body_names = {
+            mj.mj_id2name(self.model, mj.mjtObj.mjOBJ_BODY, i): i
+            for i in range(self.model.nbody)
+        }
 
-        print("[GMR] Robot Motor (Actuator) names and their IDs:")
-        self.robot_motor_names = {}
-        for i in range(self.model.nu):  # 'nu' is the number of actuators (motors)
-            motor_name = mj.mj_id2name(self.model, mj.mjtObj.mjOBJ_ACTUATOR, i)
-            self.robot_motor_names[motor_name] = i
-            if verbose:
-                print(f"Motor ID {i}: {motor_name}")
+        self.robot_motor_names = {
+            mj.mj_id2name(self.model, mj.mjtObj.mjOBJ_ACTUATOR, i): i
+            for i in range(self.model.nu)
+        }
+
+        if verbose:
+            print("[GMR] Robot Degrees of Freedom (DoF) names and their order:")
+            print(self.robot_dof_names)
+
+            print("[GMR] Robot Body names and their IDs:")
+            print(self.robot_body_names)
+
+            print("[GMR] Robot Motor (Actuator) names and their IDs:")
+            print(self.robot_motor_names)
 
         # Load the IK config
         with open(IK_CONFIG_DICT[src_human][tgt_robot]) as f:
             ik_config = json.load(f)
+
         if verbose:
             print("Use IK config: ", IK_CONFIG_DICT[src_human][tgt_robot])
 
@@ -67,14 +67,10 @@ class GeneralMotionRetargeting:
             ratio = 1.0
 
         # adjust the human scale table
-        for key in ik_config["human_scale_table"].keys():
-            ik_config["human_scale_table"][key] = (
-                ik_config["human_scale_table"][key] * ratio
-            )
+        for key in ik_config["human_scale_table"]:
+            ik_config["human_scale_table"][key] *= ratio
 
         # used for retargeting
-        self.ik_match_table1 = ik_config["ik_match_table1"]
-        self.ik_match_table2 = ik_config["ik_match_table2"]
         self.human_root_name = ik_config["human_root_name"]
         self.robot_root_name = ik_config["robot_root_name"]
         self.use_ik_match_table1 = ik_config["use_ik_match_table1"]
@@ -83,36 +79,26 @@ class GeneralMotionRetargeting:
         self.ground = ik_config["ground_height"] * np.array([0, 0, 1])
 
         self.max_iter = 10
+        self.ground_offset = 0.0
 
         self.solver = solver
         self.damping = damping
 
-        self.human_body_to_task1 = {}
-        self.human_body_to_task2 = {}
-        self.pos_offsets1 = {}
-        self.rot_offsets1 = {}
-        self.pos_offsets2 = {}
-        self.rot_offsets2 = {}
-
-        self.task_errors1 = {}
-        self.task_errors2 = {}
-
         self.ik_limits = [mink.ConfigurationLimit(self.model)]
+
         if use_velocity_limit:
-            VELOCITY_LIMITS = {k: 3 * np.pi for k in self.robot_motor_names.keys()}
+            VELOCITY_LIMITS = {k: 3 * np.pi for k in self.robot_motor_names}
             self.ik_limits.append(mink.VelocityLimit(self.model, VELOCITY_LIMITS))
 
-        self.setup_retarget_configuration()
-
-        self.ground_offset = 0.0
-
-    def setup_retarget_configuration(self):
         self.configuration = mink.Configuration(self.model)
 
         self.tasks1 = []
-        self.tasks2 = []
+        self.human_body_to_task1 = {}
+        self.pos_offsets1 = {}
+        self.rot_offsets1 = {}
+        self.task_errors1 = {}
 
-        for frame_name, entry in self.ik_match_table1.items():
+        for frame_name, entry in ik_config["ik_match_table1"].items():
             body_name, pos_weight, rot_weight, pos_offset, rot_offset = entry
             if pos_weight != 0 or rot_weight != 0:
                 task = mink.FrameTask(
@@ -130,7 +116,13 @@ class GeneralMotionRetargeting:
                 self.tasks1.append(task)
                 self.task_errors1[task] = []
 
-        for frame_name, entry in self.ik_match_table2.items():
+        self.human_body_to_task2 = {}
+        self.pos_offsets2 = {}
+        self.rot_offsets2 = {}
+        self.task_errors2 = {}
+        self.tasks2 = []
+
+        for frame_name, entry in ik_config["ik_match_table2"].items():
             body_name, pos_weight, rot_weight, pos_offset, rot_offset = entry
             if pos_weight != 0 or rot_weight != 0:
                 task = mink.FrameTask(
@@ -163,105 +155,57 @@ class GeneralMotionRetargeting:
         self.scaled_human_data = human_data
 
         if self.use_ik_match_table1:
-            for body_name in self.human_body_to_task1.keys():
-                task = self.human_body_to_task1[body_name]
+            for body_name, task in self.human_body_to_task1.items():
                 pos, rot = human_data[body_name]
                 task.set_target(
                     mink.SE3.from_rotation_and_translation(mink.SO3(rot), pos)
                 )
 
         if self.use_ik_match_table2:
-            for body_name in self.human_body_to_task2.keys():
-                task = self.human_body_to_task2[body_name]
+            for body_name, task in self.human_body_to_task2.items():
                 pos, rot = human_data[body_name]
                 task.set_target(
                     mink.SE3.from_rotation_and_translation(mink.SO3(rot), pos)
                 )
+
+    def solve(self, tasks):
+        dt = self.configuration.model.opt.timestep
+        prev_error = np.inf
+
+        for _ in range(self.max_iter):
+            vel = mink.solve_ik(
+                self.configuration,
+                tasks,
+                dt,
+                self.solver,
+                self.damping,
+                False,
+                self.ik_limits,
+            )
+            self.configuration.integrate_inplace(vel, dt)
+
+            curr_error = np.linalg.norm(
+                np.concatenate(
+                    [task.compute_error(self.configuration) for task in tasks]
+                )
+            )
+
+            if abs(prev_error - curr_error) < 0.001:
+                break
+
+            prev_error = curr_error
 
     def retarget(self, human_data, offset_to_ground=False):
         # Update the task targets
         self.update_targets(human_data, offset_to_ground)
 
         if self.use_ik_match_table1:
-            # Solve the IK problem
-            curr_error = self.error1()
-            dt = self.configuration.model.opt.timestep
-            vel1 = mink.solve_ik(
-                self.configuration,
-                self.tasks1,
-                dt,
-                self.solver,
-                self.damping,
-                False,
-                self.ik_limits,
-            )
-            self.configuration.integrate_inplace(vel1, dt)
-            next_error = self.error1()
-            num_iter = 0
-            while curr_error - next_error > 0.001 and num_iter < self.max_iter:
-                curr_error = next_error
-                dt = self.configuration.model.opt.timestep
-                vel1 = mink.solve_ik(
-                    self.configuration,
-                    self.tasks1,
-                    dt,
-                    self.solver,
-                    self.damping,
-                    False,
-                    self.ik_limits,
-                )
-                self.configuration.integrate_inplace(vel1, dt)
-                next_error = self.error1()
-                num_iter += 1
+            self.solve(self.tasks1)
 
         if self.use_ik_match_table2:
-            curr_error = self.error2()
-            dt = self.configuration.model.opt.timestep
-            vel2 = mink.solve_ik(
-                self.configuration,
-                self.tasks2,
-                dt,
-                self.solver,
-                self.damping,
-                False,
-                self.ik_limits,
-            )
-            self.configuration.integrate_inplace(vel2, dt)
-            next_error = self.error2()
-            num_iter = 0
-            while curr_error - next_error > 0.001 and num_iter < self.max_iter:
-                curr_error = next_error
-                # Solve the IK problem with the second task
-                dt = self.configuration.model.opt.timestep
-                vel2 = mink.solve_ik(
-                    self.configuration,
-                    self.tasks2,
-                    dt,
-                    self.solver,
-                    self.damping,
-                    False,
-                    self.ik_limits,
-                )
-                self.configuration.integrate_inplace(vel2, dt)
-
-                next_error = self.error2()
-                num_iter += 1
+            self.solve(self.tasks2)
 
         return self.configuration.data.qpos.copy()
-
-    def error1(self):
-        return np.linalg.norm(
-            np.concatenate(
-                [task.compute_error(self.configuration) for task in self.tasks1]
-            )
-        )
-
-    def error2(self):
-        return np.linalg.norm(
-            np.concatenate(
-                [task.compute_error(self.configuration) for task in self.tasks2]
-            )
-        )
 
     def to_numpy(self, human_data):
         for body_name in human_data.keys():
@@ -336,7 +280,6 @@ class GeneralMotionRetargeting:
             pos, quat = human_data[body_name]
             if pos[2] < lowest_pos:
                 lowest_pos = pos[2]
-                lowest_body_name = body_name
         for body_name in human_data.keys():
             pos, quat = human_data[body_name]
             offset_human_data[body_name] = [pos, quat]
